0c2d580816aaeddbd7b53a6ca51dd9d0db2c57de
robertziomek <robertziomek1993@gmail.com>
2022-08-04 20:15:19 +0200

    finish

--- main.js
+++ main.js
@@ -17,35 +17,183 @@ const padlockLogo = document.getElementById("padlock");
 const arrowDownLogo = document.querySelectorAll(".arrowDown");
 let move = 0;
 
-const description = () => {};
+const cleanMarker = () => {
+	markerList.forEach((marker) => {
+		marker.classList.add("resources__marker--none");
+	});
+};
+const cleanColor = () => {
+	resourcesList.forEach((colors) => {
+		colors.style.color = "#2f1c6a";
+	});
+};
+const cleanText = () => {
+	resourcesText.forEach((description) => {
+		description.classList.remove("resources__right-md--active");
+	});
+};
+
+const changeDiscrition = (a) => {
+	resourcesText.forEach((description) => {
+		if (a === description.dataset.texts) {
+			description.classList.add("resources__right-md--active");
+		} else description.classList.remove("resources__right-md--active");
+	});
+};
+const left = () => {
+	if (move === 0) {
+		boxes[0].style.transform = "translateX(" + 420 + "px)";
+		boxes[0].style.zIndex = "8";
+		boxes[1].style.transform = "translateX(" + 420 + "px)" + "scale(1.11)";
+		boxes[1].style.zIndex = "10";
+		boxes[2].style.transform = "translateX(" + 420 + "px)";
+		boxes[2].style.zIndex = "8";
+		boxes[3].style.transform = "translateX(" + 420 + "px)";
+		boxes[3].style.zIndex = "8";
+		boxes[4].style.transform = "translateX(" + -1680 + "px)";
+		boxes[4].style.zIndex = "8";
+		move++;
+	} else if (move === 1) {
+		boxes[0].style.transform = "translateX(" + 840 + "px)" + "scale(1.11)";
+		boxes[0].style.zIndex = "10";
+		boxes[1].style.transform = "translateX(" + 840 + "px)";
+		boxes[1].style.zIndex = "8";
+		boxes[2].style.transform = "translateX(" + 840 + "px)";
+		boxes[2].style.zIndex = "8";
+		boxes[3].style.transform = "translateX(" + -1260 + "px)";
+		boxes[3].style.zIndex = "8";
+		boxes[4].style.transform = "translateX(" + -1260 + "px)";
+		boxes[4].style.zIndex = "8";
+		move++;
+	} else if (move === 2) {
+		boxes[0].style.transform = "translateX(" + 1260 + "px)";
+		boxes[0].style.zIndex = "8";
+		boxes[1].style.transform = "translateX(" + 1260 + "px)";
+		boxes[1].style.zIndex = "8";
+		boxes[2].style.transform = "translateX(" + -840 + "px)";
+		boxes[2].style.zIndex = "8";
+		boxes[3].style.transform = "translateX(" + -840 + "px)";
+		boxes[3].style.zIndex = "8";
+		boxes[4].style.transform = "translateX(" + -840 + "px)" + "scale(1.11)";
+		boxes[4].style.zIndex = "10";
+		move++;
+	} else if (move === 3) {
+		boxes[0].style.transform = "translateX(" + 1680 + "px)";
+		boxes[0].style.zIndex = "8";
+		boxes[1].style.transform = "translateX(" + -420 + "px)";
+		boxes[1].style.zIndex = "8";
+		boxes[2].style.transform = "translateX(" + -420 + "px)";
+		boxes[2].style.zIndex = "8";
+		boxes[3].style.transform = "translateX(" + -420 + "px)" + "scale(1.11)";
+		boxes[3].style.zIndex = "10";
+		boxes[4].style.transform = "translateX(" + -420 + "px)";
+		boxes[4].style.zIndex = "8";
+		move++;
+	} else if (move === 4) {
+		boxes[0].style.transform = "translateX(" + 0 + "px)";
+		boxes[0].style.zIndex = "8";
+		boxes[1].style.transform = "translateX(" + 0 + "px)";
+		boxes[1].style.zIndex = "8";
+		boxes[2].style.transform = "translateX(" + 0 + "px)" + "scale(1.11)";
+		boxes[2].style.zIndex = "10";
+		boxes[3].style.transform = "translateX(" + 0 + "px)";
+		boxes[3].style.zIndex = "8";
+		boxes[4].style.transform = "translateX(" + 0 + "px)";
+		boxes[4].style.zIndex = "8";
+		move = 0;
+	}
+};
+const right = () => {
+	if (move === 0) {
+		boxes[0].style.transform = "translateX(" + 1680 + "px)";
+		boxes[0].style.zIndex = "8";
+		boxes[1].style.transform = "translateX(" + -420 + "px)";
+		boxes[1].style.zIndex = "8";
+		boxes[2].style.transform = "translateX(" + -420 + "px)";
+		boxes[2].style.zIndex = "8";
+		boxes[3].style.transform = "translateX(" + -420 + "px)" + "scale(1.11)";
+		boxes[3].style.zIndex = "10";
+		boxes[4].style.transform = "translateX(" + -420 + "px)";
+		boxes[4].style.zIndex = "8";
+		move = 4;
+	} else if (move === 4) {
+		boxes[0].style.transform = "translateX(" + 1260 + "px)";
+		boxes[0].style.zIndex = "8";
+		boxes[1].style.transform = "translateX(" + 1260 + "px)";
+		boxes[1].style.zIndex = "8";
+		boxes[2].style.transform = "translateX(" + -840 + "px)";
+		boxes[2].style.zIndex = "8";
+		boxes[3].style.transform = "translateX(" + -840 + "px)";
+		boxes[3].style.zIndex = "8";
+		boxes[4].style.transform = "translateX(" + -840 + "px)" + "scale(1.11)";
+		boxes[4].style.zIndex = "10";
+		move--;
+	} else if (move === 3) {
+		boxes[0].style.transform = "translateX(" + 840 + "px)" + "scale(1.11)";
+		boxes[0].style.zIndex = "10";
+		boxes[1].style.transform = "translateX(" + 840 + "px)";
+		boxes[1].style.zIndex = "8";
+		boxes[2].style.transform = "translateX(" + 840 + "px)";
+		boxes[2].style.zIndex = "8";
+		boxes[3].style.transform = "translateX(" + -1260 + "px)";
+		boxes[3].style.zIndex = "8";
+		boxes[4].style.transform = "translateX(" + -1260 + "px)";
+		boxes[4].style.zIndex = "8";
+		move--;
+	} else if (move === 2) {
+		boxes[0].style.transform = "translateX(" + 420 + "px)";
+		boxes[0].style.zIndex = "8";
+		boxes[1].style.transform = "translateX(" + 420 + "px)" + "scale(1.11)";
+		boxes[1].style.zIndex = "10";
+		boxes[2].style.transform = "translateX(" + 420 + "px)";
+		boxes[2].style.zIndex = "8";
+		boxes[3].style.transform = "translateX(" + 420 + "px)";
+		boxes[3].style.zIndex = "8";
+		boxes[4].style.transform = "translateX(" + -1680 + "px)";
+		boxes[4].style.zIndex = "8";
+		move--;
+	} else if (move === 1) {
+		boxes[0].style.transform = "translateX(" + 0 + "px)";
+		boxes[0].style.zIndex = "8";
+		boxes[1].style.transform = "translateX(" + 0 + "px)";
+		boxes[1].style.zIndex = "8";
+		boxes[2].style.transform = "translateX(" + 0 + "px)" + "scale(1.11)";
+		boxes[2].style.zIndex = "10";
+		boxes[3].style.transform = "translateX(" + 0 + "px)";
+		boxes[3].style.zIndex = "8";
+		boxes[4].style.transform = "translateX(" + 0 + "px)";
+		boxes[4].style.zIndex = "8";
+		move = 0;
+	}
+};
+const scrollPage = () => {
+	scroll1.classList.toggle("header__nav--scroll", window.scrollY > 150);
+	if (window.scrollY > 150) {
+		hostingerLogo.style.fill = "black";
+		binLogo.style.fill = "black";
+		arrowDownLogo.forEach((a) => {
+			a.style.fill = "black";
+		});
+	} else {
+		hostingerLogo.style.fill = "white";
+		binLogo.style.fill = "white";
+		arrowDownLogo.forEach((a) => {
+			a.style.fill = "white";
+		});
+	}
+};
 
 resourcesList.forEach((text) => {
-	text.addEventListener("click", () => {
+	text.addEventListener("click", (e) => {
+		cleanMarker();
+		cleanText();
+		cleanColor();
+		let check = e.target.dataset.id;
+		e.target.previousElementSibling.classList.remove(
+			"resources__marker--none"
+		);
 		text.style.color = "#673de6";
-
-		resourcesList.forEach((item) => {
-			if (text.dataset.id === item.dataset.id)
-				markerList.forEach((marker) => {
-					if (text.dataset.id === marker.dataset.id) {
-						marker.classList.remove("resources__marker--none");
-						resourcesText.forEach((description) => {
-							if (item.dataset.id === description.dataset.texts) {
-								description.classList.add(
-									"resources__right-md--active"
-								);
-							} else
-								description.classList.remove(
-									"resources__right-md--active"
-								);
-						});
-					} else {
-						marker.classList.add("resources__marker--none");
-					}
-				});
-			else {
-				item.style.color = "black";
-			}
-		});
+		changeDiscrition(check);
 	});
 });
 
@@ -79,101 +227,6 @@ arrowsrolled.forEach((arrowrolled) => {
 		});
 	});
 });
-const left = () => {
-	if (move === 0) {
-		boxes[0].style.transform = "translateX(" + 420 + "px)";
-		// boxes[1].style.transform = "scale(1.11)";
-		boxes[1].style.transform = "translateX(" + 420 + "px)";
-		boxes[2].style.transform = "translateX(" + 420 + "px)";
-		boxes[3].style.transform = "translateX(" + 420 + "px)";
-		boxes[4].style.transform = "translateX(" + -1680 + "px)";
-		move++;
-	} else if (move === 1) {
-		boxes[0].style.transform = "translateX(" + 840 + "px)";
-		boxes[1].style.transform = "translateX(" + 840 + "px)";
-		boxes[2].style.transform = "translateX(" + 840 + "px)";
-		boxes[3].style.transform = "translateX(" + -1260 + "px)";
-		boxes[4].style.transform = "translateX(" + -1260 + "px)";
-		move++;
-	} else if (move === 2) {
-		boxes[0].style.transform = "translateX(" + 1260 + "px)";
-		boxes[1].style.transform = "translateX(" + 1260 + "px)";
-		boxes[2].style.transform = "translateX(" + -840 + "px)";
-		boxes[3].style.transform = "translateX(" + -840 + "px)";
-		boxes[4].style.transform = "translateX(" + -840 + "px)";
-		move++;
-	} else if (move === 3) {
-		boxes[0].style.transform = "translateX(" + 1680 + "px)";
-		boxes[1].style.transform = "translateX(" + -420 + "px)";
-		boxes[2].style.transform = "translateX(" + -420 + "px)";
-		boxes[3].style.transform = "translateX(" + -420 + "px)";
-		boxes[4].style.transform = "translateX(" + -420 + "px)";
-		move++;
-	} else if (move === 4) {
-		boxes[0].style.transform = "translateX(" + 0 + "px)";
-		boxes[1].style.transform = "translateX(" + 0 + "px)";
-		boxes[2].style.transform = "translateX(" + 0 + "px)";
-		boxes[3].style.transform = "translateX(" + 0 + "px)";
-		boxes[4].style.transform = "translateX(" + 0 + "px)";
-		move = 0;
-	}
-};
-
-const right = () => {
-	if (move === 0) {
-		boxes[0].style.transform = "translateX(" + 1680 + "px)";
-		boxes[1].style.transform = "translateX(" + -420 + "px)";
-		boxes[2].style.transform = "translateX(" + -420 + "px)";
-		boxes[3].style.transform = "translateX(" + -420 + "px)";
-		boxes[4].style.transform = "translateX(" + -420 + "px)";
-		move = 4;
-	} else if (move === 4) {
-		boxes[0].style.transform = "translateX(" + 1260 + "px)";
-		boxes[1].style.transform = "translateX(" + 1260 + "px)";
-		boxes[2].style.transform = "translateX(" + -840 + "px)";
-		boxes[3].style.transform = "translateX(" + -840 + "px)";
-		boxes[4].style.transform = "translateX(" + -840 + "px)";
-		move--;
-	} else if (move === 3) {
-		boxes[0].style.transform = "translateX(" + 840 + "px)";
-		boxes[1].style.transform = "translateX(" + 840 + "px)";
-		boxes[2].style.transform = "translateX(" + 840 + "px)";
-		boxes[3].style.transform = "translateX(" + -1260 + "px)";
-		boxes[4].style.transform = "translateX(" + -1260 + "px)";
-		move--;
-	} else if (move === 2) {
-		boxes[0].style.transform = "translateX(" + 420 + "px)";
-		boxes[1].style.transform = "translateX(" + 420 + "px)";
-		boxes[2].style.transform = "translateX(" + 420 + "px)";
-		boxes[3].style.transform = "translateX(" + 420 + "px)";
-		boxes[4].style.transform = "translateX(" + -1680 + "px)";
-		move--;
-	} else if (move === 1) {
-		boxes[0].style.transform = "translateX(" + 0 + "px)";
-		boxes[1].style.transform = "translateX(" + 0 + "px)";
-		boxes[2].style.transform = "translateX(" + 0 + "px)";
-		boxes[3].style.transform = "translateX(" + 0 + "px)";
-		boxes[4].style.transform = "translateX(" + 0 + "px)";
-		move = 0;
-	}
-};
-const scrollPage = () => {
-	scroll1.classList.toggle("header__nav--scroll", window.scrollY > 150);
-	if (window.scrollY > 150) {
-		hostingerLogo.style.fill = "black";
-		binLogo.style.fill = "black";
-		arrowDownLogo.forEach((a) => {
-			a.style.fill = "black";
-			console.log("object");
-		});
-	} else {
-		hostingerLogo.style.fill = "white";
-		binLogo.style.fill = "white";
-		arrowDownLogo.forEach((a) => {
-			a.style.fill = "white";
-		});
-	}
-};
 
 window.addEventListener("scroll", scrollPage);
 leftDirection.addEventListener("click", left);
